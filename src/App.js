3f572f98c213b973ecb4a80247fb154717e904d0
Timothy <timothyattah733@gmail.com>
2020-06-02 06:01:48 +0100

    created edit delete and create note modal in second page component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import HomePage from './components/Homepage/HomePage';
 import LoginModal from './components/Homepage/HomePageModal/LoginModal';
 import SignUpModal from './components/Homepage/HomePageModal/SignUpModal';
 import SecondPage from './components/SecondPage/SecondPage';
+import CreateNoteModal from './components/SecondPage/SecondPageModal/CreateNoteModal';
+import DeleteModal from './components/SecondPage/SecondPageModal/DeleteModal';
+import EditModal from './components/SecondPage/SecondPageModal/EditModal';
 
 const App = () => {
   return (
@@ -14,7 +17,10 @@ const App = () => {
           <Route path="/" exact component={ HomePage } />
           <Route path="/login" component={ LoginModal } />
           <Route path="/sign_up" component={ SignUpModal } />
-          <Route path="/dash/note" component={ SecondPage} />
+          <Route path="/dash/note" component={ SecondPage } />
+          <Route path="/create/note" component={ CreateNoteModal } />
+          <Route path="/delete/note" component={ DeleteModal } />
+          <Route path="/edit/note" component={ EditModal } />
         </div>
       </Router>
     </div>
